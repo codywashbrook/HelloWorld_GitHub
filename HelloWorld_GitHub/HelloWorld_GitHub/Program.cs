5100df6c4ac7c7bb93ea2460696d07e32d940a84
codywashbrook <codywashbrook@hotmail.com>
2022-09-12 11:00:22 -0300

    Initial commit

---
+++ HelloWorld_GitHub/HelloWorld_GitHub/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_GitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cody Washbrook's Hello World GitHub Project");
+            Console.WriteLine("");
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey(true);
+
+
+        }
+    }
+}
